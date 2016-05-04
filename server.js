33f77d891d228e3201836d0ab2554daf621bf514
Benzion Blech <benzionblech@gmail.com>
2016-05-04 06:50:34 -0400

    add socket.io

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-
+io.on('connection', function(){
+	console.log("user connected via socket.io");
+});
 
 
 
